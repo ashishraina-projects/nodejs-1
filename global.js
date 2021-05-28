98f072e7e2b47e9e1628307f589e4c42393c5246
ashishrainagit2 <ashishraina55@gmail.com>
2021-05-28 10:39:46 +0530

    global object in node

---
+++ global.js
@@ -0,0 +1,18 @@
+//global object
+// console.log(global)
+
+// global.setTimeout(() => {
+//     console.log(' first setTimeout in global.setTimeout')
+// }, 6000);
+
+// setTimeout(() => {
+//     console.log('in the setTimeout')
+//     clearInterval(inter)
+// }, 6000);
+
+// const inter = setInterval(() => {
+//     console.log('in the interval')
+// }, 1000)
+
+console.log(__dirname);
+console.log(__filename);
