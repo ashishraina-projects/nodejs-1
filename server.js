5c6bcd0b17a3144ee443594e39b0de425bfc140c
ashishrainagit2 <ashishraina55@gmail.com>
2021-05-30 10:34:44 +0530

    server with file system

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) =>{
-    console.log(req.url , req.method)
-
-    // res.setHeader('Content-Type', 'text/plain')
-    // res.write('Hello Nodejs')
-    // res.end()
-
+   
+    //Set Header Content Type
     res.setHeader('Content-Type', 'text/html')
-    res.write('<head><link rel="stylesheet" href="#"></head>')
-    res.write('<h1>Hello Node </h1>')
-    res.write('<h1>Hello Node Again</h1>')
-    res.end()
+
+    //Send HTML file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        }else {
+            // res.write(data)
+            res.end(data)
+        }
+    })
 })
 
 server.listen(3000 , 'localhost' , () => {
