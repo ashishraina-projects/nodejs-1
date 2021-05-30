4f96f6e97d051a7ace86b8b1e06b86058150cd8a
ashishrainagit2 <ashishraina55@gmail.com>
2021-05-30 11:37:52 +0530

    server-router-redirection

--- server.js
+++ server.js
@@ -6,8 +6,29 @@ const server = http.createServer((req, res) =>{
     //Set Header Content Type
     res.setHeader('Content-Type', 'text/html')
 
+    let path = './views/';
+    switch(req.url){
+        
+        case '/':
+           path += 'index.html';
+           res.statusCode = 200;
+           break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+
+    }
+
     //Send HTML file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err)
             res.end()
