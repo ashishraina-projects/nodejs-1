6bc6ed71b7ff30cd70613500daa0a68b9a6190d5
ashishrainagit2 <ashishraina55@gmail.com>
2021-05-30 08:56:56 +0530

    server2

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    console.log("Response sent to Client")
+    console.log(req.url , req.method)
+
+    // res.setHeader('Content-Type', 'text/plain')
+    // res.write('Hello Nodejs')
+    // res.end()
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<head><link rel="stylesheet" href="#"></head>')
+    res.write('<h1>Hello Node </h1>')
+    res.write('<h1>Hello Node Again</h1>')
+    res.end()
 })
 
 server.listen(3000 , 'localhost' , () => {
